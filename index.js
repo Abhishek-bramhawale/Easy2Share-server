0b52ffcf454ecbba8121c8076eafdbb983cb6176
Abhishek-bramhawale <sanjivkumarbramhawale4992@gmail.com>
2025-05-30 21:36:18 +0530

    add cors confi

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const express = require('express');
-const multer = require('multer');
-const cors = require('cors');
-const path = require('path');
-const { nanoid } = require('nanoid');
-const app = express();
+import express from 'express';
+import multer from 'multer';
+import cors from 'cors';
+import { nanoid } from 'nanoid';
+import QRCode from 'qrcode';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://easy2-share-client.vercel.app/'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
@@ -55,6 +75,4 @@ app.get('/download/:code', (req, res) => {
   res.send(fileData.buffer);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
